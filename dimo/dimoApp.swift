aa7821f4e9c4de23b1fa8e6ba7de26390a89914f
Axel <a.axelvc@gmail.com>
2026-01-18 01:34:23 -0600

    Initial Commit

---
+++ dimo/dimoApp.swift
@@ -0,0 +1,17 @@
+//
+//  dimoApp.swift
+//  dimo
+//
+//  Created by Axel on 18/01/26.
+//
+
+import SwiftUI
+
+@main
+struct dimoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
